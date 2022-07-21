2105c8aab338f9892f283b53878f73b3a0ab38ca
ilnariki97 <88076392+ilnariki97@users.noreply.github.com>
2022-07-21 16:51:59 +0400

    commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer__container">
+                <button className="footer__button">Поиск</button>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
